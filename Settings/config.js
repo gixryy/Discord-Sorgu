40edbade4198d374dbf8e735921d77e8b766e2da
Atahan <76434430+AtahanBERT@users.noreply.github.com>
2023-05-11 17:18:41 +0200

    Add files via upload

---
+++ Settings/config.js
@@ -0,0 +1,38 @@
+﻿module.exports = {
+
+    token: "", //Bot tokeni
+    owners: [""], //Bot sahipleri
+    guildID: "",
+
+    roller: {
+
+        SORGUCU: "1087505959007567872", //Sorgucu rolü
+
+    },
+
+    emojiler: {
+
+        UPLOAD: "" || "📑", //Upload emojisi
+
+    },
+    
+    api: {
+
+        ADSOYAD: "https://perlaservis.net/api/adsoyad?auth=servis&", //Ad soyad api
+        AD: "https://perlaservis.net/api/ad?auth=servis&ad=", //Ad api
+        SOYAD: "https://perlaservis.net/api/soyad?auth=servis&soyad=", //Soyad api
+        TCKN: "https://perlaservis.net/api/tcpro/auth=servis&tc=", //Tc api
+        TCKNPRO: "https://perlaservis.net/api/tcprov31/auth=servis&tc=", //Serino api
+        AILE: "https://perlaservis.net/api/aile/auth=servis&tc=", //Aile api
+        SULALE: "https://perlaservis.net/api/sulale/auth=servis&tc=", //Sulale api
+        EOKUL: "https://perlaservis.net/api/eokul/auth=servis&tc=", //E-okul api
+        AOL: "https://perlaservis.net/api/aolvesika/auth=servis&tc=", //Aöl api
+        ADRES: "https://perlaservis.net/api/ikametgah/auth=servis&tc=", //Adres api
+        TCGSM: "https://perlaservis.net/api/tcgsm/auth=servis&tc=", //Tc gsm api
+        GSMTC: "https://perlaservis.net/api/gsmtc/auth=servis&gsm=", //Gsm tc api
+        ASI: "https://perlaservis.net/api/asi/auth=servis&tc=", //Aşı api
+        ILAC: "https://perlaservis.net/api/ilac/auth=servis&tc=", //İlaç api
+
+    }
+
+}
