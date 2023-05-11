79aa262d782ca20285a3eaa15de598826a4b3d65
Atahan <76434430+AtahanYLDZ@users.noreply.github.com>
2023-05-11 17:53:21 +0200

    Update config.js

--- Settings/config.js
+++ Settings/config.js
@@ -18,20 +18,20 @@
     
     api: {
 
-        ADSOYAD: "https://perlaservis.net/api/adsoyad?auth=servis&", //Ad soyad api
-        AD: "https://perlaservis.net/api/ad?auth=servis&ad=", //Ad api
-        SOYAD: "https://perlaservis.net/api/soyad?auth=servis&soyad=", //Soyad api
-        TCKN: "https://perlaservis.net/api/tcpro/auth=servis&tc=", //Tc api
-        TCKNPRO: "https://perlaservis.net/api/tcprov31/auth=servis&tc=", //Serino api
-        AILE: "https://perlaservis.net/api/aile/auth=servis&tc=", //Aile api
-        SULALE: "https://perlaservis.net/api/sulale/auth=servis&tc=", //Sulale api
-        EOKUL: "https://perlaservis.net/api/eokul/auth=servis&tc=", //E-okul api
-        AOL: "https://perlaservis.net/api/aolvesika/auth=servis&tc=", //Aöl api
-        ADRES: "https://perlaservis.net/api/ikametgah/auth=servis&tc=", //Adres api
-        TCGSM: "https://perlaservis.net/api/tcgsm/auth=servis&tc=", //Tc gsm api
-        GSMTC: "https://perlaservis.net/api/gsmtc/auth=servis&gsm=", //Gsm tc api
-        ASI: "https://perlaservis.net/api/asi/auth=servis&tc=", //Aşı api
-        ILAC: "https://perlaservis.net/api/ilac/auth=servis&tc=", //İlaç api
+        ADSOYAD: "https://perlaservis.net", //Ad soyad api
+        AD: "https://perlaservis.net", //Ad api
+        SOYAD: "https://perlaservis.net", //Soyad api
+        TCKN: "https://perlaservis.net", //Tc api
+        TCKNPRO: "https://perlaservis.net", //Serino api
+        AILE: "https://perlaservis.net", //Aile api
+        SULALE: "https://perlaservis.net", //Sulale api
+        EOKUL: "https://perlaservis.net", //E-okul api
+        AOL: "https://perlaservis.net", //Aöl api
+        ADRES: "https://perlaservis.net", //Adres api
+        TCGSM: "https://perlaservis.net", //Tc gsm api
+        GSMTC: "https://perlaservis.net", //Gsm tc api
+        ASI: "https://perlaservis.net", //Aşı api
+        ILAC: "https://perlaservis.net", //İlaç api
 
     }
 
